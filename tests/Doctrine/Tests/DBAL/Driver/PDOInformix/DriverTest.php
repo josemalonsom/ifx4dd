4a54fa168a876a600b53af4f61b1fa52276f2aff
Jose M. Alonso M <josemalonsom@yahoo.es>
2015-09-06 20:39:20 +0200

    Added more tests.

---
+++ tests/Doctrine/Tests/DBAL/Driver/PDOInformix/DriverTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Doctrine\Tests\DBAL\Driver\PDOInformix;
+
+use Doctrine\DBAL\Driver\PDOInformix\Driver;
+use Doctrine\Tests\DBAL\Driver\AbstractInformixDriverTest;
+
+class DriverTest extends AbstractInformixDriverTest
+{
+    public function testReturnsName()
+    {
+        $this->assertSame('pdo_informix', $this->driver->getName());
+    }
+
+    protected function createDriver()
+    {
+        return new Driver();
+    }
+
+    /**
+     * @dataProvider exceptionMessagesOnMissingParameters
+     */
+    public function testThrowsExceptionOnMissingParameter(
+        array $params, $exceptionMessage
+    ) {
+
+        $this->setExpectedException(
+            'Doctrine\DBAL\DBALException', $exceptionMessage
+        );
+
+        $this->driver->connect($params);
+    }
+
+    public function exceptionMessagesOnMissingParameters()
+    {
+        $data = array();
+
+        $data['missing dbname'] = array(
+            $this->getConnectionParamsWithoutKey('dbname'),
+            "Missing 'dbname' in configuration for informix driver"
+        );
+
+        $data['missing host'] = array(
+            $this->getConnectionParamsWithoutKey('host'),
+            "Missing 'host' in configuration for informix driver"
+        );
+
+        $data['missing protocol'] = array(
+            $this->getConnectionParamsWithoutKey('protocol'),
+            "Missing 'protocol' in configuration for informix driver"
+        );
+
+        $data['missing server'] = array(
+            $this->getConnectionParamsWithoutKey('server'),
+            "Missing 'server' in configuration for informix driver"
+        );
+
+        return $data;
+    }
+
+    private function getConnectionParamsWithoutKey($excludedKey)
+    {
+        $allParams = array(
+            'dbname'   => 'dbname',
+            'host'     => 'host',
+            'protocol' => 'protocol',
+            'server'   => 'server',
+        );
+
+        $params = array();
+
+        foreach($allParams as $key => $value) {
+            if ($key != $excludedKey) {
+                $params[$key] = $value;
+            }
+        }
+
+        return $params;
+    }
+}
---
+++ tests/Doctrine/Tests/DBAL/Functional/Driver/PDOInformix/ConnectionTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Doctrine\Tests\DBAL\Functional\Driver\PDOInformix;
+
+use Doctrine\Tests\DbalFunctionalTestCase;
+
+class ConnectionTest extends DbalFunctionalTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $this->connection = $this->_conn->getWrappedConnection();
+    }
+
+    public function testGetServerVersion()
+    {
+        $this->assertRegExp(
+            '/IBM Informix Dynamic Server Version [0-9]{2}\.[0-9]{2}\.(F|H|T|U)[[:alnum:]]+/',
+            $this->connection->getServerVersion()
+        );
+    }
+
+    public function testQuote()
+    {
+        $this->assertSame(1, $this->connection->quote(1, \PDO::PARAM_INT));
+        $this->assertSame("'a'", $this->connection->quote("a"));
+        $this->assertSame("'a'", $this->connection->quote("a", \PDO::PARAM_STR));
+    }
+}
