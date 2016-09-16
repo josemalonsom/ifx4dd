4cda80e20be8d9804db0f7e48ff3d5372d39150b
Jose M. Alonso M <josemalonsom@yahoo.es>
2016-09-16 23:38:31 +0200

    Updated DataAccessTest with changes in DBAL

--- tests/Doctrine/Tests/DBAL/Functional/DataAccessTest.php
+++ tests/Doctrine/Tests/DBAL/Functional/DataAccessTest.php
@@ -239,6 +239,12 @@ class DataAccessTest extends \Doctrine\Tests\DbalFunctionalTestCase
         $this->assertEquals('foo', $row[1]);
     }
 
+    public function testFetchNoResult()
+    {
+        self::assertFalse(
+            $this->_conn->executeQuery('SELECT test_int FROM fetch_table WHERE test_int = ?', array(-1))->fetch()
+        );
+    }
     public function testFetchAssoc()
     {
         $sql = "SELECT test_int, test_string FROM fetch_table WHERE test_int = ? AND test_string = ?";
